f0abef36f6e07512958d25b426cbd77160488856
berkeoz <84924357+berkeoz@users.noreply.github.com>
2022-05-26 18:34:40 -0400

    M

--- test.sql
+++ test.sql
@@ -1,8 +1,3 @@
 insert into "test" values('Test');
 select sysdate from dual;
-<<<<<<< HEAD
-commit;
-=======
-select sysdate from dual;
->>>>>>> parent of fc0ae88 (Revert "Revert "Revert "Testing""")
 commit;
