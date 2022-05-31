e6c080a5d4ed1cf3e3a16eb2c92d66cc921b566e
berkeoz <84924357+berkeoz@users.noreply.github.com>
2022-05-30 20:51:22 -0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,7 +2,6 @@ def buildApp() {
     echo 'building the application...'
 } 
 
-def issueKey = currentBuild.getBuildCauses('org.marvelution.jji.trigger.JiraCause')[0]['issueKey']
 
 def testApp() {
     echo 'testing the application...'
